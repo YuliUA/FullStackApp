9bbb5773724dd5e4971c403e9503586e775326a7
Yuliya Hiryak <shyx85@gmail.com>
2019-05-04 00:00:29 +0300

    mvc

--- client/src/layouts/DeleteDate.js
+++ client/src/layouts/DeleteDate.js
@@ -1,21 +1,21 @@
 import React from 'react'
 
-export default function DeleteDate({submit, change, toggleForm}) {
+export default function DeleteDate({ submit, change, toggleForm }) {
   return (
     <form onSubmit={submit}>
-            <div className="form-group p-3">
-              <button type="button" className="close" aria-label="Close" onClick={toggleForm}>
-                <span aria-hidden="true">&times;</span>
-              </button>
-              <label htmlFor="deleted_date">Set the date</label>
-              <input type="date"
-                className="form-control"
-                id="deleted_date"
-                name="date"
-                onChange={change} />
-              <input type="submit" value="Confirm" className="btn btn-danger mt-3" />
-            </div>
-          </form>
+      <div className="form-group p-3">
+        <button type="button" className="close" aria-label="Close" onClick={toggleForm}>
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <label htmlFor="deleted_date">Set the date</label>
+        <input type="date"
+          className="form-control"
+          id="deleted_date"
+          name="date"
+          onChange={change} />
+        <input type="submit" value="Confirm" className="btn btn-danger mt-3" />
+      </div>
+    </form>
   )
 }
 
--- client/src/layouts/Homepage.js
+++ client/src/layouts/Homepage.js
@@ -7,7 +7,7 @@ export default class Homepage extends Component {
       <div>
         <h1 className="text-center bg-dark text-light p-3 mb-5">Hi, I'm Your Store App</h1>
         <div className="container">
-        <p>If you want to add some products click this button below &#8595;</p>
+          <p>If you want to add some products click this button below &#8595;</p>
           <Link to='/purchase' className="btn btn-lg btn-primary">Purchase products</Link>
           <br />
           <p className="mt-4">If you want to get report click this button below &#8595;</p>
